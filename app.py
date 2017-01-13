8e6620d271582bf09c5cd4d7773a63eaec51bf75
Travis <travissheppard.1988@gmail.com>
2017-01-12 20:13:39 -0800

    xbox controller config

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
   return render_template('index.html')
+
+# make static assets available
+@app.route('/public/<path:path>')
+def send_static(path):
+    return send_from_directory('public', path)
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
